4f3dd748ab2f2e46e3083a7a32f14abc0bbe45bc
JamelAuto <jimmy.mro@outlook.fr>
2021-05-20 17:47:46 +0200

    API OK and Back button

--- app/src/main/java/com/example/esieajava/Activity2.java
+++ app/src/main/java/com/example/esieajava/Activity2.java
@@ -2,11 +2,28 @@ package com.example.esieajava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class Activity2 extends AppCompatActivity {
-
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +31,11 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
         Bundle b = getIntent().getExtras();
         int id = b.getInt("id");
+        textViewResult = findViewById(R.id.countryBody);
+
         DisplayCountryName(id);
+        makeApiCall(id);
+        backButton();
     }
 
     public void DisplayCountryName(final int position){
@@ -22,5 +43,58 @@ public class Activity2 extends AppCompatActivity {
                 ,"Lettonie","Malte","Méxique","Népal","Rwanda","Serbie","Singapoure","Espagne","Togo","Uruguay"};
         final TextView countryName = (TextView) findViewById(R.id.countryDetail);
         countryName.setText(arrayCountrie[position]);
+    }
+
+    static final String BASE_URL = "https://jamelauto.github.io/Data/";
+    private void makeApiCall(final int position){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CountryApi CountryApi = retrofit.create(CountryApi.class);
+
+        Call<List<Country>> call = CountryApi.getCountryResponse();
+
+        call.enqueue(new Callback<List<Country>>() {
+            @Override
+            public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
+                if (!response.isSuccessful()){
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Country> restCountryResponses = response.body();
+                String content = ("Indicatif Téléphonique : " + restCountryResponses.get(position).getPhone() + "\n\n") + ("Nom de domaine : " + restCountryResponses.get(position).getWeb() + "\n\n") + ("Langue officielle : " + restCountryResponses.get(position).getLanguage() + "\n\n");
+                textViewResult.append(content);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Country>> call, Throwable t) {
+                showError();
+            }
+        });
+
+    }
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API ERROR", Toast.LENGTH_SHORT).show();
+    }
+
+    public void backButton() {
+        Button button = (Button) findViewById(R.id.backButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/esieajava/MainActivity.java
+++ app/src/main/java/com/example/esieajava/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements  RecyclerViewClic
         setContentView(R.layout.activity_main);
 
         showList();
-        makeApiCall();
     }
 
     private void showList(){
@@ -50,46 +49,8 @@ public class MainActivity extends AppCompatActivity implements  RecyclerViewClic
         recyclerView.setAdapter(adapter);
     }
 
-    static final String BASE_URL = "https://jamelauto.github.io/Data/";
-    private void makeApiCall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        CountryApi CountryApi = retrofit.create(CountryApi.class);
-
-        Call<RestCountryResponse> call = CountryApi.getCountryResponse();
-        call.enqueue(new Callback<RestCountryResponse>() {
-            @Override
-            public void onResponse(Call<RestCountryResponse> call, Response<RestCountryResponse> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    List<Country> countryList = response.body().getResults();
-                    Toast.makeText(getApplicationContext(), "API SUCCESS", Toast.LENGTH_SHORT).show();
-
-                }else{
-                    showError();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RestCountryResponse> call, Throwable t) {
-                showError();
-            }
-        });
-
-    }
-    private void showError() {
-        Toast.makeText(getApplicationContext(), "API ERROR", Toast.LENGTH_SHORT).show();
-    }
-
     @Override
     public void onItemClick(int position) {
-        Toast.makeText(this, images[position], Toast.LENGTH_SHORT).show();
         openActivity2(position);
     }
 
