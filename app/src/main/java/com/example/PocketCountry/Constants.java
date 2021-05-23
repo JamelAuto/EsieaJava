f3fd5332c924606a23ffe45cd2a53c186b7ea287
JamelAuto <jimmy.mro@outlook.fr>
2021-05-23 11:17:06 +0200

    Implement Data Cache

---
+++ app/src/main/java/com/example/PocketCountry/Constants.java
@@ -0,0 +1,7 @@
+package com.example.PocketCountry;
+
+public class Constants {
+    static String KEY_COUNTRY = "jsonCountryList";
+    static String KEY_APP = "pocketCountry";
+    static String KEY_GIT = "https://jamelauto.github.io/Data/";
+}
